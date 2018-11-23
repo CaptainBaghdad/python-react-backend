2f78e329de781a78a8defc2b78cbd9f3ad978645
me <juniorjr083@gmail.com>
2018-11-22 20:43:38 -0500

    init commit

---
+++ stocks/api/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from stocks.models import User
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User 
+        fields = ('name', 'email')
